77d02fb9a7411c220c20eb8d072c0278911a3685
tecarto3 <tecarto@gmail.com>
2025-02-22 12:57:44 +0900

    1-3

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
   const closePopup = () => setPopupType("");
 
   return (
-    <Router basename="/bfds">
+    <Router>
       <Preloader load={load} />
       <div className={`App ${load ? "no-scroll" : "scroll"}`}>
         <Navbar
